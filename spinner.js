928edd94266ef0d99184132a30bd8926dbe93152
ayesha08a <87036204+ayesha08a@users.noreply.github.com>
2021-07-09 23:20:05 +0600

    spinner game

---
+++ spinner.js
@@ -0,0 +1,15 @@
+function spinfunction(){
+    var x = 1024;
+    var y = 9999;
+
+    var deg = Math.floor(Math.random() * (x - y))+y;
+
+    document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+
+    var element = document.getElementById('mainbox');
+    element.classList.remove('animate');
+    setTimeout(function(){
+
+        element.classList.add('animate')
+    }, 5000);
+}
